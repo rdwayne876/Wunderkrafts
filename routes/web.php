59d94ccdda1c012bd75651b2a73a9bf05a60ec96
Dwayne Robinson <rdwayne876@gmail.com>
2020-06-18 02:20:22 -0500

    Guarded Admin Authentication

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('/admin')->namespace('Admin')->group(function(){
     
     Route::match(['get','post'], '/', "AdminController@login");
-    Route::get('dashboard', 'AdminController@dashboard');
+
+    Route::group(['middleware' =>  ['admin']], function() {
+        Route::get('dashboard', 'AdminController@dashboard');
+    });
+    
 
 });
