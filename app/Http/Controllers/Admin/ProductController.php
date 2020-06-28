741c2d86f3f9b55981a16859185386949a4d0cc2
Dwayne Robinson <rdwayne876@gmail.com>
2020-06-27 20:58:22 -0500

    Show Products list

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,8 +4,44 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Product;
+use Session;
 
 class ProductController extends Controller
 {
-    //
+    public function products() {
+        $products = Product::get();
+
+        $products = json_decode(json_encode($products));
+        // echo "<pre>"; print_r($products); die;   
+        
+        return view('admin.products.products')->with(compact('products'));
+    
+    }
+
+    public function updateProductStatus(Request $request) {
+        if($request->ajax()) {
+            $data = request()->all();
+
+            if($data['status'] == "Active") {
+                $status = 0;
+            } else {
+                $status = 1;
+            }
+
+            Product::where('id', $data['product_id'])
+                ->update(['status'=>$status]);
+
+            return response()->json(
+                ['status'=>$status, 'product_id'=>$data['product_id']]);
+        }
+    }
+
+    public function delete($id) {
+        //Find and delete product
+        Product::where('id', $id)->delete();
+
+        Session::flash('success', 'Product deleted successfully');
+        return redirect()->back();
+    }
 }
